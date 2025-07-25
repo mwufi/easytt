4a470da5a5a2d9d57c0fc1b428108ada4f71e627
Zen T <ztang230@gmail.com>
2025-07-25 12:35:55 -0500

    yokey

---
+++ sst.config.ts
@@ -0,0 +1,23 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+import { statfsSync } from "fs";
+
+export default $config({
+  app(input) {
+    return {
+      name: "my-ts-app",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    const api = new sst.aws.ApiGatewayV2("api");
+    api.route("GET /", {
+      handler: "index.upload",
+    });
+    api.route("GET /latest", {
+      handler: "index.latest",
+    });
+  },
+});
